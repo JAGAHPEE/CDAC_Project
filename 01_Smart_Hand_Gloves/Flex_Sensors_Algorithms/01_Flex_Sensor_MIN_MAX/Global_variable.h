4983255079dc2fe919d1e9384807f53c7fa7f8fa
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2024-02-27 10:55:05 +0530

    Delete 01_Smart_Hand_Gloves/Flex_Sensors_Algorithms directory

--- 01_Smart_Hand_Gloves/Flex_Sensors_Algorithms/01_Flex_Sensor_MIN_MAX/Global_variable.h
+++
@@ -1,9 +0,0 @@
-#define test_count 4
-
-int MAX_1 = 0;
-int MAX_2 = 0;
-int MAX_3 = 0;
-int MIN_1 = 0;
-int MIN_2 = 0;
-int MIN_3 = 0;
-
--- 01_Smart_Hand_Gloves/Flex_Sensors_Algorithms/09_Motions_and_sleep_mode_OLED_Sound_ENUM_DEBUG_02/Debug_Macro.h
+++
@@ -1,6 +0,0 @@
-#define DEBUG
-#ifdef DEBUG
-#define DEBUG_LN(x) Serial.println(x)
-#else
-#define DEBUG_LN(x) while(0){}
-#endif
